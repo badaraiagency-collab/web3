ebb3ce1a8d7e07f81cd283083c91676abd687189
badaraiagency-collab <badaraiagency@gmail.com>
2025-11-26 09:45:05 +0500

    git add app/layout.tsx git commit -m "Add Tawk.to chat widget" git push

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter, Poppins, JetBrains_Mono } from "next/font/google"
 import { AuthProvider } from "@/lib/auth-context"
 import { NavigationProvider } from "@/lib/navigation-context"
 import "./globals.css"
+import Script from "next/script"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -48,6 +49,25 @@ export default function RootLayout({
             {children}
           </NavigationProvider>
         </AuthProvider>
+
+        {/* Tawk.to Chat Widget */}
+        <Script
+          id="tawkto"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6926844f188b0a1960fc1227/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   )
